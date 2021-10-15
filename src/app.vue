7947cf3d7041e0b534e563393e22665ae0ecc46a
describano8 <describano8@gmail.com>
2021-10-15 17:16:29 +0200

    commit 1

---
+++ src/app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img alt="logo del proyecto" src="./images/LOGO.JPG">
+    <HolaMundo msg="¡Hola Mundo!"/>
+  </div>
+</template>
+
+<script>
+import HolaMundo from './components/Test.vue'
+export default {
+  name: 'App',
+  components: {
+    HolaMundo
+  }
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './app.vue'
+
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue);
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.config.productionTip = false
+
+
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
